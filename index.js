c5f8775d7b20aa1cee00a389220963c4681f9c4d
sndpokhrel <sandypokhrel09@gmail.com>
2024-01-20 23:08:49 +0545

    updated packages and rerouted dashboard

--- index.js
+++ index.js
@@ -1,12 +1,41 @@
+//Express
 const express = require("express");
 const app = express();
-
+// Config file
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
+// Packages
+const morgan = require("morgan");
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+const db = require("./utility/db");
 
-app.set("view engine", "ejs");
+// Morgan setup
+app.use(morgan("dev"));
 
-app.use("/", require("./router/login.routes"));
+// Set view engine
+app.set("view engine", [path.join(__dirname, "views")]);
+app.use("/public", express.static("public"));
 
-const PORT = process.env.PORT || 9325;
-app.listen(PORT, console.log(`Server started at port ${PORT}`));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    saveUnitialized: true,
+    resave: true,
+  })
+);
+
+app.use("/login", require("./router/login.routes"));
+app.use("/", require("./router/dashboard.routes"));
+
+db.then(() => {
+  const PORT = process.env.PORT;
+  app.listen(PORT, console.log(`Server started at port ${PORT}`));
+}).catch((err) => {
+  console.log(err);
+});
